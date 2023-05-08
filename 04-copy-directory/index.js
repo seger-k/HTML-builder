6fb771cb771121efa74fbbbacad5883cdd8f820d
Sergey <segany4@gmail.com>
2023-05-09 00:11:41 +0300

    feat: 04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const file = path.join(__dirname, 'files');
+const fileCopy = path.join(__dirname, 'files-copy');
+
+fs.rm(fileCopy, { recursive: true, force: true }, err => {
+  if (err) console.log(err);
+  else {
+    fs.readdir(file, { withFileTypes: true }, (err, data) => {
+      if (err) console.log(err);
+      fs.mkdir(fileCopy, { recursive: true, force: true }, err => {
+        if (err) console.log(err);
+        data.forEach(chunk => {
+          const chunkFile = path.join(__dirname, 'files', chunk.name);
+          const chunkFileCopy = path.join(__dirname, 'files-copy', chunk.name)
+          fs.copyFile(chunkFile, chunkFileCopy, err => {
+            if (err) console.log(err);
+          });
+        });
+      });
+    });
+  }
+});
 
